edc060d72950612b9958544a22d24c9cd91d818c
gabrielss97 <gabriel.ssalvador97@gmail.com>
2022-03-03 16:31:45 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+const boxes = document.querySelectorAll(".box");
+
+boxes.forEach((box) => {
+  box.addEventListener("click", () => {
+    const title = box.firstElementChild;
+    const text = box.lastElementChild;
+    const arrow = box.firstElementChild.lastElementChild;
+
+    if (!title.classList.contains("bold")) {
+      title.classList.add("bold");
+      text.style.display = "block";
+      arrow.style.transform = "rotate(180deg)";
+    } else {
+      title.classList.remove("bold");
+      text.style.display = "none";
+      arrow.style.transform = "rotate(0deg)";
+    }
+  });
+});
